a1fb0db7fbe6916d8f9a119ec756da39aee400fc
Varshant Atray <92811825+varshant-atray@users.noreply.github.com>
2021-10-30 00:13:07 +0530

    TO-DO-LIST
    As you take notes and plan projects in OneNote, you might wonder how to manage deadlines and remember the things on your to-do list. One way is to create Outlook tasks. Then you can view and track those tasks in Outlook and even get reminders.

---
+++ app/src/androidTest/java/com/example/Dataflair/dataflairtodolist/ExampleInstrumentedTest.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    
+    public static void main(String args[]) {
+        // your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+
+    }
+}
